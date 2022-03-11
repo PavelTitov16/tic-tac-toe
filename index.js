dd185da9b67923512cc1097d5dde0b6237363af6
PavelTitov16 <pashoktitov16@gmail.com>
2022-03-11 14:26:45 +0300

    feat: add game turns

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const field = document.querySelector('.field');
 let turn = 0;
 
 field.addEventListener('click', event => {
-    if(event.target.className === 'square')
-    console.log(event.target);
+    if(event.target.className === 'square') {
+        if (turn % 2 === 0) {
+            event.target.innerHTML = 'X';
+        } else {
+            event.target.innerHTML = '0';
+        }
+        turn++;
+    }
+    
+
 })
